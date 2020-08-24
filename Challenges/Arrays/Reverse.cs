b51bf1868df9e999b5b5ace6c1db979b7dfd3efc
Nathan Kinkade <nkkinkade@gmail.com>
2020-08-24 14:07:21 -0500

    initial push

---
+++ Challenges/Arrays/Reverse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.Arrays
+{
+    class Reverse
+    {
+        static int[] ReverseArray(int[] array)
+        {
+            return array;
+        }
+    }
+}
