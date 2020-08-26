dbb812229825b08f539012bc3502fa0e04afbe12
Nathan Kinkade <nkkinkade@gmail.com>
2020-08-26 15:41:24 -0500

    created binary search and tests using Thoery

---
+++ Challenges.test/Arrays/BinarySearchTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using Challenges.Arrays;
+
+namespace Challenges.test.Arrays
+{
+    public class BinarySearchTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 4, 5 }, 3, -1)]
+        [InlineData(new int[] { 1, 3, 5, 18, 22 }, 3, 1)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5, 4)]
+        public void TestBinarySearch(int[] arr, int searchValue, int expected)
+        {
+            int result = BinarySearch.Search(arr, searchValue);
+            Assert.Equal(expected, result);
+        }
+    }
+}
